9ba4114d0aa61714f7517712d247cd49d3850855
Juan Z <juancarlosz1150@gmail.com>
2022-01-05 09:46:59 -0400

    hoy 4

--- laso/models/models.py
+++ laso/models/models.py
@@ -73,7 +73,7 @@ class CustomEmploye(models.Model): #
     _inherit = 'hr.employee' # nos permite modificar el modelo que vamos a especificar
     
     zone2 = fields.Selection([('34', '34'), ('35', '35'), ('36', '36'),('37', '37'),('38', '38'),('39', '39'),('40', '40'),('41', '41'),('42', '42'),('43', '43'),('44', '44'),('45', '45'),('46', '46')], string='Zapatos')
-    zone3 = fields.Selection([('26', '26'),('27', '27'),('28', '28'),('29', '29'),('30', '30'),('32', '32'),('33', '33'),('34', '34'),('35', '35'),('36', '36'),('37', '37'),('38', '38'),('39', '39'),('40', '40')], string='Pantalon')
+    zone3 = fields.Selection([('26', '26'),('28', '28'),('30', '30'),('32', '32'),('34', '34'),('36', '36'),('38', '38'),('40', '40')], string='Pantalon')
     zone4 = fields.Selection([('X', 'XS'), ('S', 'S'), ('M', 'M'),('L', 'L'),('XL', 'XL'),('XXL', 'XXL')], string='Camisa')
     zone5 = fields.Selection([('X', 'XS'), ('S', 'S'), ('M', 'M'),('L', 'L'),('XL', 'XL'),('XXL', 'XXL')], string='Chemise') 
     zone6 = fields.Selection([('A','Alimentos'),('F','Fármacos'),('As','Asma alérgico'),('Anl','Animales'),('Da','Dermatitis atópica'),('Pn','Poliposis nasal'),('Ra','Rinitis alérgica'),('Uc','Urticaria'),('Niq','Níquel'),('Sus','Sustancias'),('Ot','Otras'),('Ni','Ninguna')], string='Tipo de alergia')
@@ -82,5 +82,4 @@ class CustomEmploye(models.Model): #
     vacuna = fields.Selection([('S','Si'),('N','No')])
     dosis= fields.Text()
     ndosis=fields.Selection([('1','1'),('2','2'),('3','3')])
-   
-    
+  
